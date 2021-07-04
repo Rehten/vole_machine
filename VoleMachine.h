1c2195ae3bdae3797e71199beebfa976b3dad9c6
Paumpeero <Paumpeero@rambler.ru>
2021-07-04 21:17:00 +0300

    Initial commit

---
+++ VoleMachine.h
@@ -0,0 +1,14 @@
+//
+// Created by rehten on 04.07.2021.
+//
+
+#ifndef VOLE_MACHINE_VOLEMACHINE_H
+#define VOLE_MACHINE_VOLEMACHINE_H
+
+class VoleMachine
+{
+private:
+  short unsigned int registers_[16]: {}
+};
+
+#endif //VOLE_MACHINE_VOLEMACHINE_H
